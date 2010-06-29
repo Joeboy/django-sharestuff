447b861ce3bf3d95f459abd6408372e9798b7543
Joe Button <ml_github@joebutton.co.uk>
2010-06-29 12:03:34 +0100

    Lots of stuff - get project to a state where it's possible for users to register and add offers.

--- settings.py
+++ settings.py
@@ -1,24 +1,11 @@
 # Django settings for sharestuff project.
+import os
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+DEV_SERVER = False
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+PROJECT_PATH = os.path.realpath(os.path.split(__file__)[0])
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -27,11 +14,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/London'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
 
 SITE_ID = 1
 
@@ -45,20 +32,20 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'f1+wibbmkz317gytz4j)sv&sw!*v5+)g^ax%jq%n@9knpfa7zf'
+SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -78,10 +65,9 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'sharestuff.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, "templates"),
 )
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -89,11 +75,18 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'sharestuff.userprofile',
+    'sharestuff.things',
+    'registration',
 )
 
+AUTH_PROFILE_MODULE = 'userprofile.UserProfile'
+
+# Time that account activation keys will remain active:
+ACCOUNT_ACTIVATION_DAYS = 3
+
 try:
-    import localsettings
+    from localsettings import *
 except ImportError:
     pass
--- urls.py
+++ urls.py
@@ -1,17 +1,29 @@
 from django.conf.urls.defaults import *
+from settings import DEV_SERVER, MEDIA_ROOT
+from userprofile.models import create_userprofile
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^sharestuff/', include('sharestuff.foo.urls')),
+    (r'^$', 'sharestuff.views.home'),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+#    (r'^stuff/', include('things.urls')),
+    (r'^accounts/profile/$', 'django.views.generic.simple.redirect_to', {'url':'/userprofile/',}),
+    (r'^accounts/register/$', 'registration.views.register', {'profile_callback':create_userprofile,}),
+    (r'^accounts/', include('registration.urls')),
+    (r'^userprofile/', include ('userprofile.urls')),
+    (r'^~(?P<username>\w+)/', 'things.views.user_stuff_list'),
 
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
+
+
+if DEV_SERVER:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                                  {'document_root': MEDIA_ROOT,
+                                   'show_indexes': True}),
+    )
+
