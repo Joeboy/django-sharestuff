14c27de71a1b83d70cd2a28c14ebf8387354fa79
buttoj3 <joe.button@aviva.co.uk>
2011-06-29 16:19:42 +0100

    Add local offer images to admin

--- goingspare/offers/admin.py
+++ goingspare/offers/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from offers.models import LocalOffer, OfferCategory
+from offers.models import LocalOffer, OfferCategory, LocalOfferImage
 
 class LocalOfferAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_time_added'
@@ -8,6 +8,10 @@ class LocalOfferAdmin(admin.ModelAdmin):
 class OfferCategoryAdmin(admin.ModelAdmin):
     pass
 
+class LocalOfferImageAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(LocalOffer, LocalOfferAdmin)
 admin.site.register(OfferCategory, OfferCategoryAdmin)
+admin.site.register(LocalOfferImage, LocalOfferImageAdmin)
 
