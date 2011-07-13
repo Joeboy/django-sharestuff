572bd1b376ec5f274b35e1caecbb162fa5b1c6f6
buttoj3 <joe.button@aviva.co.uk>
2011-07-13 18:33:00 +0100

    Fix link for rss feed

--- goingspare/offers/feeds.py
+++ goingspare/offers/feeds.py
@@ -1,14 +1,14 @@
 from userprofile.models import UserProfile
 from django.shortcuts import get_object_or_404
-from django.utils.feedgenerator import Atom1Feed
 from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse
 
 class UserActivityFeed(Feed):
     def get_object(self, request, username):
         return get_object_or_404(UserProfile, user__username=username)
 
-    def link(self, user):
-        return user.get_absolute_url()
+    def link(self, userprofile):
+        return reverse('user-offers', kwargs={'username':userprofile.user.username})
 
     def item_title(self, item):
         return item.title
