d13392a88decec2cf4102195e202612c7affd8d3
buttoj3 <joe.button@aviva.co.uk>
2011-07-17 12:47:55 +0100

    Fix dodgy import in tasks file

--- goingspare/offers/tasks.py
+++ goingspare/offers/tasks.py
@@ -1,10 +1,8 @@
-from .importing.scrape_norwichfreegle import grab, get_offers
 from celery.decorators import task
+from django.core import management
 
 @task()
 def scrape():
     print "doing the task!"
-    html = grab()
-    offers = get_offers(html)
-    print offers
+    management.call_command('scrape_norwichfreecycle')
     
