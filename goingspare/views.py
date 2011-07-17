dc3b98b35b5a8633d6b457269ed996cf3e824d88
buttoj3 <joe.button@aviva.co.uk>
2011-07-17 14:34:45 +0100

    Add offer listing to the front page. Tweak some text.

--- goingspare/views.py
+++ goingspare/views.py
@@ -1,6 +1,13 @@
-from goingspare.utils import render_to_response_context
 from django.db import connection
 from django import forms
 
+from goingspare.utils import render_to_response_context
+from userprofile.models import UserProfile
+from offers.models import LocalOffer
+
 def index(request):
-    return render_to_response_context(request, 'index.html')
+    userprofile = UserProfile.get_for_user(request.user)
+    offers = LocalOffer.objects.filter_by_user(userprofile)
+    return render_to_response_context(request,
+                                      'index.html',
+                                      {'offers': offers})
